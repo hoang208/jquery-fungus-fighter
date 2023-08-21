662b4570fb8c1a0f647f0871f848217a13fb1002
Paul HoangLong <paulhoanglong@gmail.com>
2023-08-21 17:13:17 -0500

    Completed required tasks

--- script.js
+++ script.js
@@ -5,14 +5,68 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+let apValue = 100;
+let hpValue = 100;
+
 function onReady() {
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
-    
+    render();
     
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+
+    $('.arcane-scepter').on('click', arcaneScepterAttack)
+    $('.entangle').on('click', entangleAttack)
+    $('.dragon-blade').on('click', dragonBladeAttack)
+    $('.star-fire').on('click', starFireAttack)
+}
+//end onReady()
+
+function arcaneScepterAttack(event){
+    event.preventDefault();
+    apValue-=17;
+    hpValue-=14;
+    render();
+}
+//end arcaneScepterAttack()
+
+function entangleAttack(event) {
+    event.preventDefault();
+    apValue-=23;
+    hpValue-=9;
+    render();
+}
+//end entangleAttack()
+
+function dragonBladeAttack(event) {
+    event.preventDefault();
+    apValue-=38;
+    hpValue-=47;
+    render();
+}
+//end dragonBladeAttack()
+
+function starFireAttack(event) {
+    event.preventDefault();
+    apValue-=33;
+    hpValue-=25;
+    render();
+}
+//end starFireAttack()
+
+function render() {
+    if (hpValue <= 0) {
+        hpValue = 0;
+        $('.freaky-fungus').removeClass('walk').addClass('dead');
+    } if (apValue <= 0) {
+        apValue = 0;
+        $('.freaky-fungus').removeClass('walk').addClass('jump');
+        $('.attack-btn').attr("disabled", true);
+    }
+    $('.ap-text').text(apValue + " AP");
+    $('.hp-text').text(hpValue + " HP");
 }
